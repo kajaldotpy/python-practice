6fd95227f005d14f446495fb02d1cf4ffef770f6
kajaldotpy <cskajalmain@gmail.com>
2023-02-12 18:35:22 +0530

    check

---
+++ 1sum.py
@@ -0,0 +1,11 @@
+#Add two number
+"""
+a=10
+b=50
+c=a+b
+print(f"sum of two number is:{c}")"""
+
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=a+b
+print(f"sum of two number is:{c}")
